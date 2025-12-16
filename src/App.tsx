04dc2f6dc345356410ff52bd36559f7b819e8ea5
ikawho <andrii.ikaliuk@gmail.com>
2025-12-16 02:20:24 +0100

    V3.0 ADDED CHARTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { useEffect, useState } from "react";
-import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import FileUploadButton from "./components/FileUploadButton";
 import ActivitiesChart from "./components/ActivitiesChart";
 
 function App() {
-	const { user, signOut } = useAuthenticator();return ( <main> <h1>Your ride analyzer</h1> <h3>{user?.signInDetails?.loginId}</h3> <FileUploadButton buttonText="+ UPLOAD" /> <ActivitiesChart /> <div style={{ marginTop: "12px" }}> App successfully hosted. Try uploading a .fit-File </div> <button onClick={signOut} style={{ marginTop: "24px" }}> Sign out </button> </main> ); } export default App;
+  const { user, signOut } = useAuthenticator();
+
+  return (
+    <main>
+      <h1>Your ride analyzer</h1>
+      <h3>{user?.signInDetails?.loginId}</h3>
+
+      <FileUploadButton buttonText="+ UPLOAD" />
+      <ActivitiesChart />
+
+      <div style={{ marginTop: "12px" }}>
+        App successfully hosted. Try uploading a .fit-File
+      </div>
+
+      <button onClick={signOut} style={{ marginTop: "24px" }}>
+        Sign out
+      </button>
+    </main>
+  );
+}
+
+export default App;
